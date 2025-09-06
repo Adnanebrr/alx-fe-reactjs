c58c688ba776308bafcb34fe80eb69a0037015ef
Adnane Birrouz <birrouzadnane67@gmail.com>
2025-09-06 14:18:29 +0100

    Task 2: Refactor to Context API with final structure

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,15 +3,16 @@ import UserContext from './UserContext';
 import UserProfile from './components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const user = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <UserContext.Provider value={userData}>
+    <UserContext.Provider value={user}>
       <UserProfile />
     </UserContext.Provider>
   );
 }
 
 export default App;
+
 
 
