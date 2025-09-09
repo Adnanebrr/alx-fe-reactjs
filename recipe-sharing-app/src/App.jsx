c7cd0b210e49f8f04aefa96e3ecca2ad8f2d4617
Adnane Birrouz <birrouzadnane67@gmail.com>
2025-09-09 16:55:42 +0100

    Task1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Recipe Sharing App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            } />
+            <Route path="/recipe/:id" element={<RecipeDetails />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
